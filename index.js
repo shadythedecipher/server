8f6ea436b81778c52eac6b38ef5175049733cb88
shadrack <shadrak.abely321@gmail.com>
2022-12-02 17:12:14 +0300

    helo prince

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Wocrld');
+  res.send('Hellofgf Wocrld');
 });
 
 app.listen(PORT, HOST, () => {

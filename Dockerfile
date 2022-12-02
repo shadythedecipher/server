6395a9f874da090013ed1ac6d2f35c3d8f09aa07
shadrack <shadrak.abely321@gmail.com>
2022-12-02 15:45:08 +0300

    new update with doocker

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:16
+
+WORKDIR /usr/src/app
+
+
+COPY package*.json ./
+
+RUN npm install
+# If you are building your code for production
+RUN npm ci --only=production
+
+COPY . .
+
+EXPOSE 8080
+
+CMD [ "npm", "start" ]
--- index.js
+++ index.js
@@ -18,6 +18,27 @@
 // app.use(attendanceRouter);
 const df = "juma";
 console.log("jusdmgdga");
+
+
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
+
+
+
 // mongoose
 //   .connect(DB)
 //   .then(() => {
